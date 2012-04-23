1a27fbec3f45ac963ed601d3eb542bab63831951
Kaspars <kaspars@metronet.no>
2012-04-23 15:11:06 +0200

    Forche-check for updates only when WP_GITHUB_FORCE_UPDATE is true

--- updater/updater.php
+++ updater/updater.php
@@ -47,7 +47,7 @@ class WPGitHubUpdater {
 
 		$this->set_defaults();
 
-		if ( ( defined('WP_DEBUG') && WP_DEBUG ) || ( defined('WP_GITHUB_FORCE_UPDATE') || WP_GITHUB_FORCE_UPDATE ) )
+		if ( defined('WP_GITHUB_FORCE_UPDATE') || WP_GITHUB_FORCE_UPDATE )
 			add_action( 'admin_init', array( $this, 'delete_transients' ) );
 		
 		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'api_check' ) );
